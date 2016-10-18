1000b168bd35fc158cc8a7ee75ff2e4e7828b0a7
Julian Weiland <jw@werbeagenten.de>
2016-10-18 14:07:00 +0200

    missing reference added

--- includes/import-templates.php
+++ includes/import-templates.php
@@ -66,7 +66,7 @@ class WPFPI_IMPORT_TEMPLATES {
 
 	private function album_template() {
 		add_post_meta( $this->new_post_id, 'attachement_type', $this->fb_post_attachement['type'], true );
-		foreach ($subattachments as $subattachment) {
+		foreach ($this->subattachments as $this->subattachment) {
 			$this->img_attachment_ids[] = $this->import_image_from_url( $subattachment['media']['image']['src'], $subattachment['target']['id'] );
 		}
 		add_post_meta( $this->new_post_id, 'img_attachment_ids', $this->img_attachment_ids );
